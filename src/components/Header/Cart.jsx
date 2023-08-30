46e1f9bd5811d7c1e2e9fb59ec5f91e205a4abba
KierranyC <kierranyc@outlook.com>
2023-08-29 19:26:27 -0500

    starting cart

--- src/components/Header/Cart.jsx
+++ src/components/Header/Cart.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 
-// This component renders the cart. I think this is pretty self explanatory, but this component should have a list of products picked out by the customer, with the price and quantity to be purchased. The quantity should be modifiable and the subtotal should update to reflect those changes. Their should be a field for customers to add payment information, and for users to save payment information if they wish to for future orders.
+// This component renders the cart. I think this is pretty self explanatory, 
+// but this component should have a list of products picked out by the customer, 
+// with the price and quantity to be purchased.The quantity should be modifiable 
+// and the subtotal should update to reflect those changes.Their should be a field 
+// for customers to add payment information, and for users to save payment information 
+// if they wish to for future orders.
 const Cart = () => {
   return (
     <div>
